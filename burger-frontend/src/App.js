9ba9060a806aafabd6ddccd80840925cdaa89ad4
dochi <dj043045@gmail.com>
2023-05-10 17:59:37 +0900

    front main page

--- burger-frontend/src/App.js
+++ burger-frontend/src/App.js
@@ -10,22 +10,26 @@ import Signup from './pages/Signup';
 import Store from './pages/Store';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Event from './pages/Event';
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/inquiry" element={<Inquiry />} />
-        <Route exact path="/menu" element={<Menu />} />
-        <Route exact path="/mypage" element={<MyPage />} />
-        <Route exact path="/signin" element={<Signin />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/store" element={<Store />} />
-      </Routes>
+      <div className="main-content">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/inquiry" element={<Inquiry />} />
+          <Route exact path="/menu" element={<Menu />} />
+          <Route exact path="/mypage" element={<MyPage />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/store" element={<Store />} />
+          <Route exact path="/event" element={<Event />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
